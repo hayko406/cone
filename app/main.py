05717e549333d71a561353c2106a5efd6fa245ae
hayko406 <gayk.t@mail.ru>
2024-08-01 00:11:05 +0300

    fix: обработка неправильного ввода

--- app/main.py
+++ app/main.py
@@ -8,14 +8,21 @@ d = st.number_input('Меньший диаметр')
 
 def calculate(L, D, d):
     if d > D:
-        return 'Маленький диаметр не может быть больше большого'
+        return 'Меньший диаметр не может быть больше большего'
     a = (D-d)/2
     c = (a**2+L**2)**.5
-    corner = asin(a/c)*180/pi 
+    try:
+        corner = asin(a/c)*180/pi
+    except ZeroDivisionError:
+        return 'Размер 0 недопустим'
+     
     return corner
 
 with st.form('myform'):
     button = st.form_submit_button(label='Посчитать угол')
     if button:
-        ans = round(calculate(L, D, d),3)
-        st.write(f'Угол составит {ans} градусов')
+        ans = calculate(L, D, d)
+        if isinstance(ans, float):
+            st.write(f'Угол составит {round(ans, 3)} градусов')
+        else:
+            st.write(ans)
